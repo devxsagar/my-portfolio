e1e24c2054c2ffcfea07c0d70cbde93a8cc250fa
Sagar Mitra <mitrasagar10@gmail.com>
2025-10-03 12:11:31 +0530

    feat: add responsive navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import React from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { Outlet } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
+    <div className="min-h-screen max-w-screen bg-bg-primary  font-suisse-regular">
       <Navbar />
       <main>
         <Outlet />
       </main>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="border border-border fixed top-5 left-1/2 -translate-x-1/2 rounded-4xl w-[calc(100%-30px)] xl:w-[1200px] min-h-12 flex items-center justify-between px-5">
+      <h2 className="font-metamorphous text-3xl">sm.</h2>
 
-export default Navbar
+      {/* Nav links */}
+      <div className="text-sm gap-5 lg:text-base flex lg:gap-8">
+        <Link to="/projects">Projects</Link>
+        <Link>Resume</Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
