8d28b031c1daf88df8f0c71189e9d3c90db9a98e
Sagar Mitra <mitrasagar10@gmail.com>
2025-10-10 20:27:15 +0530

    update: remove navbar form about me page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import ReactLenis from "lenis/react";
 
 const App = () => {
+  const location = useLocation();
+
+  // Add routes where you DON'T want Navbar
+  const hideNavbarRoutes = ["/about-me"];
+
+  const shouldHideNavbar = hideNavbarRoutes.includes(location.pathname);
+
   return (
-    <ReactLenis root options={{
-      lerp: 0.08
-    }}>
+    <ReactLenis
+      root
+      options={{
+        lerp: 0.08,
+      }}
+    >
       <div className="relative min-h-screen max-w-screen bg-bg-primary  font-suisse-regular">
-        <Navbar />
+        {!shouldHideNavbar && <Navbar />}
         <main>
           <Outlet />
         </main>
