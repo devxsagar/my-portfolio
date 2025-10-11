178b20f26fe2a0671cde4abee6fcaf0cb1d885e7
Sagar Mitra <mitrasagar10@gmail.com>
2025-10-11 12:32:51 +0530

    add: navbar to about me page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,9 @@
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import ReactLenis from "lenis/react";
 
 const App = () => {
-  const location = useLocation();
-
-  // Add routes where you DON'T want Navbar
-  const hideNavbarRoutes = ["/about-me"];
-
-  const shouldHideNavbar = hideNavbarRoutes.includes(location.pathname);
-
   return (
     <ReactLenis
       root
@@ -19,7 +12,7 @@ const App = () => {
       }}
     >
       <div className="relative min-h-screen max-w-screen bg-bg-primary  font-suisse-regular">
-        {!shouldHideNavbar && <Navbar />}
+        <Navbar />
         <main>
           <Outlet />
         </main>
