0820ecc494a1f984bb2708c42bb44707e5c5d3e2
Sagar Mitra <mitrasagar10@gmail.com>
2025-10-29 19:15:25 +0530

    add: microsoft clarity

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,16 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { Outlet } from "react-router-dom";
 import ReactLenis from "lenis/react";
+import { useEffect } from "react";
+import { clarity } from "react-microsoft-clarity";
 
 const App = () => {
+  useEffect(() => {
+    if(process.env.NODE_ENV === "production") {
+      clarity.init("")
+    }
+  }, []);
+
   return (
     <ReactLenis
       root
