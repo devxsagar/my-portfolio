8314f87053af105ef0dfcb03033e931b17808ea2
Sagar Mitra <mitrasagar10@gmail.com>
2025-10-31 21:07:08 +0530

    fix: nav for mobile and tab ui

--- src/components/MobileNavLink.jsx
+++ src/components/MobileNavLink.jsx
@@ -1,13 +1,14 @@
 import { motion } from "motion/react";
 import { Link } from "react-router-dom";
 import { navLinks } from "../utils/constants";
+import { X } from "lucide-react";
 
 const MobileNavLink = () => {
   return (
-    <div className="absolute top-12 -right-8 t-0 w-screen md:w-screen h-[80vh] bg-bg-primary rounded-lg flex flex-col items-center gap-y-5 md:gap-y-10 px-10 py-20 text-2xl md:text-4xl tracking-wide ">
+    <div className="absolute -top-8 -right-8  w-screen md:w-screen h-screen bg-bg-primary rounded-lg flex flex-col items-center gap-y-5 md:gap-y-10 px-10 py-50 text-2xl md:text-4xl tracking-wide ">
       {navLinks.map((link, index) => {
         return (
-          <Link key={index} to={link.href} >
+          <Link key={index} to={link.href}>
             <motion.span
               initial={{ y: -20, opacity: 0 }}
               animate={{
@@ -27,6 +28,17 @@ const MobileNavLink = () => {
           </Link>
         );
       })}
+
+      <motion.span
+        className="inline-block absolute top-9 right-8"
+        initial={{ rotate: 0 }}
+        whileTap={{
+          rotate: 180,
+          transition: { duration: 0.1, ease: "linear" },
+        }}
+      >
+        <X className=" z-50 md:w-12 md:h-8" />
+      </motion.span>
     </div>
   );
 };
