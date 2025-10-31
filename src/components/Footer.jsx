9254011fe524d8170beba38bcb8d7d0327e2faf0
Sagar Mitra <mitrasagar10@gmail.com>
2025-10-31 17:40:56 +0530

    update: footer and add confetti on copy button click

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
+import { motion } from "motion/react";
 import { email, footerLinks } from "../utils/constants";
 import FooterLink from "./FooterLink";
+import confetti from "canvas-confetti";
 
 const Footer = () => {
   const [copied, setCopied] = useState(false);
@@ -9,11 +11,18 @@ const Footer = () => {
     if (email) {
       navigator.clipboard.writeText(email);
       setCopied(true);
+      confetti({
+        particleCount: 100,
+        spread: 300,
+        startVelocity: 15,
+        scalar: 0.9,
+      });
     }
 
     setTimeout(() => {
       setCopied(false);
-    }, 1000);
+      // confetti.reset()
+    }, 3000);
   };
 
   return (
@@ -34,21 +43,32 @@ const Footer = () => {
           }}
         />
 
+        <motion.div
+          className="absolute inset-0 z-50"
+          style={{
+            background: "#fafafa",
+          }}
+          initial={{ height: "100%" }}
+          whileInView={{ height: 0 }}
+          transition={{ duration: 1, ease: "linear" }}
+          viewport={{ once: true, amount: 0.3 }}
+        />
+
         <div
           className="flex flex-col justify-between
-        w-[calc(100%-30px)] xl:min-w-[1200px] xl:w-[1200px] min-h-[28rem] md:min-h-[38rem] lg:min-h-[40rem] mx-auto relative z-10"
+        w-[calc(100%-30px)] xl:min-w-[1200px] xl:w-[1200px] min-h-[28rem] md:min-h-[38rem] lg:min-h-[25rem] mx-auto relative z-10"
         >
           {/* Top */}
-          <div className="pt-1 md:pt-10 flex items-start justify-between  max-lg:flex-col max-lg:items-center max-lg:gap-15 relative z-10">
-            <div className="flex flex-col">
-              <p className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl tracking-wide">
-                Let's connect?
+          {/* <div className="pt-1 md:pt-10 flex items-center justify-center  max-lg:flex-col max-lg:items-center max-lg:gap-15 relative z-10">
+            <div>
+              <p className="text-2xl sm:text-3xl md:text-4xl lg:text-6xl font-suisse-book tracking-wide uppercase">
+                let's connect
               </p>
             </div>
-          </div>
+          </div> */}
 
-          {/* Middle */}
-          <div className="py-2 flex flex-col items-center gap-y-15">
+          {/* Top */}
+          <div className="pt-1 md:pt-10 flex flex-col justify-center items-center gap-y-15">
             <div className="flex items-center justify-evenly md:justify-between max-md:gap-4 w-fit sm:w-md pl-7 pr-3 py-2 rounded-full bg-bg-black/90">
               <p className="text-sm md:text-lg font-suisse-book opacity-60 tracking-widest">
                 {email}
@@ -63,7 +83,7 @@ const Footer = () => {
 
             <div className="flex items-center gap-15 overflow-y-hidden px-5">
               {footerLinks.map((link, index) => {
-                return <FooterLink key={index} {...link} />
+                return <FooterLink key={index} {...link} />;
               })}
             </div>
           </div>
@@ -91,3 +111,12 @@ export default Footer;
 </div>
 
  */
+
+// <div className="flex items-center justify-center">
+//   <motion.span className="text-[12rem] font-suisse-bold text-text-grey"
+//   initial={{ letterSpacing: "-0.07em" }}
+//   whileInView={{letterSpacing: "0.1em", transition: {duration: 0.8, ease: "easeInOut"}}}
+//   >
+//     devxsagar
+//   </motion.span>
+// </div>
